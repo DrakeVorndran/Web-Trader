c3de01c2fe0508f0c6461313c30f783c31ead4f1
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2019-01-10 10:43:32 -0800

    crates work, better way to asign/create items

---
+++ items/text_colors.js
@@ -0,0 +1,9 @@
+const text_colors = [
+    {name: "Blue Text color", value: ".blue-font"},
+    {name: "Red Text color", value: ".red-font"},
+    {name: "Green Text color", value: ".green-font"},
+    {name: "Black Text color", value: ".black-font"},
+    {name: "White Text color", value: ".white-font"},
+]
+
+module.exports = text_colors;
